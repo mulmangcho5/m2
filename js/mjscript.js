40b0ab02313fbfc0989f694037ecde0cbb1b1846
mulmangcho5 <88593691+mulmangcho5@users.noreply.github.com>
2021-10-20 14:37:29 +0900

    Add files via upload

---
+++ js/mjscript.js
@@ -0,0 +1,11 @@
+function openNav(){
+    $("#mySidenav").addClass("showLeftMenu");
+}
+
+function closeNav(){
+    $("#mySidenav").removeClass("showLeftMenu")
+}
+
+$(document).ready(function(){
+    
+})
