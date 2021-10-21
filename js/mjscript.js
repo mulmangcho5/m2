86d56ebad8d3af79fd86df9fec5bcf6617d4c4eb
mulmangcho5 <88593691+mulmangcho5@users.noreply.github.com>
2021-10-21 10:06:58 +0900

    Add files via upload

--- js/mjscript.js
+++ js/mjscript.js
@@ -1,3 +1,5 @@
+
+// 햄버거 메뉴
 function openNav(){
     $("#mySidenav").addClass("showLeftMenu");
 }
@@ -6,6 +8,27 @@ function closeNav(){
     $("#mySidenav").removeClass("showLeftMenu")
 }
 
+
+
+
+// 스와이퍼
 $(document).ready(function(){
-    
-})
+    const swiper = new Swiper('.swiper-container',{
+
+        direction: 'horizontal',
+        slidesPerView: 1,
+        spaceBetewwn: 30,
+        debugger: true,
+        loop: true,
+        centeredSlides: true,
+
+        autoplay : {
+            delay: 4000,
+            disableOnInteraction: false,
+        },
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        }
+    });
+});
