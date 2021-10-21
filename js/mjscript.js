19b692dd7a1643e46ecb26afd3dba9996c6a631b
mulmangcho5 <88593691+mulmangcho5@users.noreply.github.com>
2021-10-21 14:58:10 +0900

    Add files via upload

--- js/mjscript.js
+++ js/mjscript.js
@@ -11,7 +11,7 @@ function closeNav(){
 
 
 
-// 스와이퍼
+// 메인 상단 1번 스와이퍼
 $(document).ready(function(){
     const swiper = new Swiper('.swiper-container',{
 
@@ -31,4 +31,35 @@ $(document).ready(function(){
             clickable: true,
         }
     });
+
+
+
+
+    //2번 -바닐라자바스크립트
+
+
+
+
+    // 3번째 스와이퍼 5개짜리
+    const swiper3 = new Swiper('.swiper-container5',{
+
+        direction: 'horizontal',
+        slidesPerView: 5,       //1번이랑 다름
+        spaceBetewwn: 30,
+        debugger: true,
+        loop: true,
+        centeredSlides: true,
+
+        autoplay : {
+            delay: 3000,
+            disableOnInteraction: false,
+        },
+        navigation: {
+            //이전페이지
+            prevEl: '.swiper-button-prev',
+            //다음페이지
+            nextEl: '.swiper-button-next'
+        }
+    });
+
 });
